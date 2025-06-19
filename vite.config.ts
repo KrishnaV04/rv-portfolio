a45a15dc4d6c6c63b2da02fa2e8693c9aa76aad9
Radhakrishna Vempati <vempatir@uci.edu>
2025-06-18 17:53:59 -0700

    modified routing, vite and package for correct routing with deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/rv-portfolio/',
   server: {
-    // base: '/rv-portfolio/',
     host: "::",
     port: 8080,
   },
